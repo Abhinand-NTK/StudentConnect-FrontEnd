2bed6b28b2c71246a9cdd945dea242384c76de12
AbhinandNtk <abhinandntkm@gmail.com>
2025-01-18 14:31:35 +0530

    SW-3:Setup the Redux Store and Theme Store

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import store from './store/configureStore.js'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from '@material-tailwind/react'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider>
+      <Provider store={store}>
         <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
