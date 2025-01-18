fbab984bee08a213595470e6bc692d812f89950f
AbhinandNtk <abhinandntkm@gmail.com>
2025-01-18 13:48:39 +0530

    SW-2: ReStrucutre the folders

--- src/assets/ProfilePicDefault/Propic.js
+++
@@ -1,2 +0,0 @@
-
-export const defaultProPic = 'https://st4.depositphotos.com/11574170/25191/v/450/depositphotos_251916955-stock-illustration-user-glyph-color-icon.jpg'
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast';
-import { AuthContext, Context } from './context/contex.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Context>
         <App />
-      </Context>
-      <Toaster />
-    </BrowserRouter>
   </React.StrictMode>,
 )
